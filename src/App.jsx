ec4b88dc15043e73b8cdbe78b4ffb40ce9e9750f
afiimiese23 <nur24si@mahasiswa.pcr.ac.id>
2026-03-30 16:05:06 +0700

    pertemuan3

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BiodataDiri from './components/BiodataDiri';
+
+function App() {
+  return (
+    <div>
+      <BiodataDiri />
+    </div>
+  );
+}
+
+export default App;
