15dbe727532be029eb12a53051b7a3546c7100eb
afiimiese23 <nur24si@mahasiswa.pcr.ac.id>
2026-05-03 23:23:30 +0700

    Tugass Pertemuan7

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -7,7 +7,6 @@ export default function AuthLayout() {
             
             <div className="bg-white p-8 rounded-2xl shadow-lg w-full max-w-md">
 
-                {/* Logo */}
                 <div className="flex flex-col items-center justify-center mb-6">
                     <div className="flex items-center gap-2 text-yellow-600 text-3xl font-bold">
                         <FaHotel />
@@ -18,10 +17,8 @@ export default function AuthLayout() {
                     </span>
                 </div>
 
-                {/* Form (Login/Register) */}
                 <Outlet />
 
-                {/* Footer */}
                 <p className="text-center text-sm text-gray-500 mt-6">
                     © 2026 LuxuryStay Hotel. All rights reserved.
                 </p>
